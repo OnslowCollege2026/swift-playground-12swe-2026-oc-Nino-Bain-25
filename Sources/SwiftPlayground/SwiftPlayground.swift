85105637c77681fa9651087f8e78c4e90a6df9e6
oc-Nino-Bain-25 <nino.bain@student.onslow.school.nz>
2026-03-11 22:14:19 +0000

    Wow it was that simple

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,14 +1,22 @@
-///Adds up all integers in a collumn in a 2D array. Does not include rows that 
-/// do not have a value in that column.
+///Adds up all integers in a collumn in a 2D array. Does not include rows that do not have a value in that column.
+/// Parametres:
+/// Table: The 2d array of numbers we are looking at
+/// column: The collumn of numbers in the table we are looking at.
+
 func collumnTotal (table: [[Int]], column: Int) -> Int {
-var collumnTotality = 0
-table[column].forEach {row in
-if table[column][row] =
+var counter = 0
+
+for row in table {
+if row.count >= column {
+    counter = counter + row[column]
+}
 }
 
 
 
-return 0
+
+
+return counter
 }
 
 
@@ -21,20 +29,20 @@ return 0
 struct SwiftPlayground {
     static func main() {
 let temperatures = [
-    [2.0, 4.0, 6.0],
-    [8.0, 10.0, 12.0],
-    [14.0, 16.0, 18.0]
+    [2, 4, 6],
+    [8, 10, 12],
+    [14, 16, 18]
 ]
 
 print(temperatures[0])
 print(temperatures[1][2])
 print(temperatures[2][1])
-var averagedtemp = 0.0
+var averagedtemp = 0
 temperatures[1].forEach {temp in
 averagedtemp = averagedtemp + temp
 
 }
-averagedtemp = averagedtemp / 3
+averagedtemp = averagedtemp / temperatures[1].count
 
 
 print(averagedtemp)
@@ -54,6 +62,6 @@ let table = [
     [8, 6, 1,],
     [10]
     ]
-    collumnTotal(table: table, column: collumns)
+    print(collumnTotal(table: table, column: collumns))
 
     }}
