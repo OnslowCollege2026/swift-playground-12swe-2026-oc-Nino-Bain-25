e1643766d17b49949a50ef21d66373e1bcdab773
oc-Nino-Bain-25 <nino.bain@student.onslow.school.nz>
2026-03-16 23:44:06 +0000

    BattleShip attempt.

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,26 +1,16 @@
-///Adds up all integers in a collumn in a 2D array. Does not include rows that do not have a value in that column.
-/// Parametres:
-/// Table: The 2d array of numbers we are looking at
-/// column: The collumn of numbers in the table we are looking at.
+///Asks the user for a number
+func coordInput() -> Int{
+if let userInput = readLine(), let coord = Int(userInput) {
+return coord
 
-func collumnTotal (table: [[Int]], column: Int) -> Int {
-var counter = 0
-
-for row in table {
-if row.count >= column {
-    counter = counter + row[column]
 }
+else {
+    return 0
 }
 
 
 
-
-
-return counter
 }
-
-
-
 
 
 
@@ -28,40 +18,33 @@ return counter
 
 struct SwiftPlayground {
     static func main() {
-let temperatures = [
-    [2, 4, 6],
-    [8, 10, 12],
-    [14, 16, 18]
-]
+    var board: [[String]]
+    let size = 6
+    board = Array(repeating: Array(repeating: "~", count: size), count: size)
+    var guesses = Array(repeating: Array(repeating: "~", count: size), count: size)
+    
+    board[1][1] = "S"
+    board[2][1] = "S"
+    board[3][1] = "S"
+    board[5][5] = "S"
+    board[5][4] = "S"
+    board[5][3] = "S"
+    board[5][2] = "S"
+    board[2][3] = "S"
+    board[3][3] = "S"
+   var correctGueses = 0
+while correctGueses != 9 {
+guesses.forEach { row in
+    [row].forEach {number in
+    print(number)
 
-print(temperatures[0])
-print(temperatures[1][2])
-print(temperatures[2][1])
-var averagedtemp = 0
-temperatures[1].forEach {temp in
-averagedtemp = averagedtemp + temp
+
+
+    }
+    }
+    
 
 }
-averagedtemp = averagedtemp / temperatures[1].count
 
-
-print(averagedtemp)
-var counter = 0
-for row in temperatures {
-for value in row {
-    print(value)
-    counter = counter + 1
-}
-
-}
-print(counter)
-var collumns = 0
-let table = [
-    [3, 5, 7, 9],
-    [2, 4],
-    [8, 6, 1,],
-    [10]
-    ]
-    print(collumnTotal(table: table, column: collumns))
 
     }}
