58a8e7ac9471317cad6ef0cef644c026f881311c
oc-Nino-Bain-25 <nino.bain@student.onslow.school.nz>
2026-03-17 00:14:55 +0000

    What the heck is updog

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,11 +4,28 @@ if let userInput = readLine(), let coord = Int(userInput) {
 return coord
 
 }
-else {
-    return 0
+
+return 7
+
+
 }
 
+func PrintBoard(_ board: [[String]]) {
+    var columnlables = ""
+    for i in 0...board.count {
+        columnlables = columnlables + "\(i)"
+    }
+    print (columnlables)
+}
 
+func processGuess(row: Int, col: Int, Board: [[String]], guesses: [[String]]) -> [[String]] {
+guard row >= 1, row <= Board.count, col >= 1, col <= Board[0].count else {
+print("INVALID GUESS")
+return guesses
+}
+guard guesses[row - 1][col - 1] == "O" || guesses[row - 1][col - 1] == "X" else {
+    print("You already guessed that spot, try again.")
+}
 
 }
 
@@ -18,11 +35,14 @@ else {
 
 struct SwiftPlayground {
     static func main() {
+        //Creates the board you ar playing on
     var board: [[String]]
     let size = 6
     board = Array(repeating: Array(repeating: "~", count: size), count: size)
-    var guesses = Array(repeating: Array(repeating: "~", count: size), count: size)
+//Creates the variable that the board
+    var guesses = board
     
+    //Places ships for player to find
     board[1][1] = "S"
     board[2][1] = "S"
     board[3][1] = "S"
@@ -32,7 +52,10 @@ struct SwiftPlayground {
     board[5][2] = "S"
     board[2][3] = "S"
     board[3][3] = "S"
-   var correctGueses = 0
+
+func printBoard(board: board)
+
+var correctGueses = 0
 while correctGueses != 9 {
 guesses.forEach { row in
     [row].forEach {number in
