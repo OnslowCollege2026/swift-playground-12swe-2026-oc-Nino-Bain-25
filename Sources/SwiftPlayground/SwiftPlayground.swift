c5201124ffdf5a0402687169b26ec88d53cc9e15
oc-Nino-Bain-25 <nino.bain@student.onslow.school.nz>
2026-03-18 22:14:09 +0000

    I dids its

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,73 +1,144 @@
-///Asks the user for a number
-func coordInput() -> Int{
-if let userInput = readLine(), let coord = Int(userInput) {
-return coord
-
-}
-
-return 7
-
-
-}
-
-func PrintBoard(_ board: [[String]]) {
-    var columnlables = ""
-    for i in 0...board.count {
-        columnlables = columnlables + "\(i)"
-    }
-    print (columnlables)
-}
-
-func processGuess(row: Int, col: Int, Board: [[String]], guesses: [[String]]) -> [[String]] {
-guard row >= 1, row <= Board.count, col >= 1, col <= Board[0].count else {
-print("INVALID GUESS")
-return guesses
-}
-guard guesses[row - 1][col - 1] == "O" || guesses[row - 1][col - 1] == "X" else {
-    print("You already guessed that spot, try again.")
-}
-
-}
-
-
-
+import Foundation
 @main
-
 struct SwiftPlayground {
-    static func main() {
-        //Creates the board you ar playing on
-    var board: [[String]]
-    let size = 6
-    board = Array(repeating: Array(repeating: "~", count: size), count: size)
-//Creates the variable that the board
-    var guesses = board
-    
-    //Places ships for player to find
-    board[1][1] = "S"
-    board[2][1] = "S"
-    board[3][1] = "S"
-    board[5][5] = "S"
-    board[5][4] = "S"
-    board[5][3] = "S"
-    board[5][2] = "S"
-    board[2][3] = "S"
-    board[3][3] = "S"
+    static func main() throws {
+        // The maximum number of guesses the user can make.
+        let maximumGuesses = 6
+        
+        // The size of the board (width and height).
+        let size = 6
+        
+        //The amount of ships on the board
+        let shipCount = 4
 
-func printBoard(board: board)
+        // The board you are playing on.
+        let ocean = randomShipping (size: size, shipCount: shipCount)
+        
+        // A record of the guesses that have been made.
+        var guesses = Array(repeating: Array(repeating: "~", count: size), count: size)
+        
 
-var correctGueses = 0
-while correctGueses != 9 {
-guesses.forEach { row in
-    [row].forEach {number in
-    print(number)
-
-
-
+        printBoard(guesses)
+        printBoard(ocean)
+        // Allow the user to make a certain number of guesses.
+        var progress = 1
+        while progress < maximumGuesses {
+            // Ask for the row and column number.
+            print("\(progress): Please enter a row number, press Enter, then a column number: ")
+            
+            // Get the user's input for both the row and column.
+            // If the input is not usable, tell them to try again.
+            guard let userInput = readLine(),
+                  let row = Int(userInput),
+                  let userInput2 = readLine(),
+                  let col = Int(userInput2) else {
+                print("Your guesses are invalid. Please try again.")
+                continue
+            }
+            
+            // By this point in the code, the row and col must be valid integers.
+            // Use processGuess to check that they are also valid guesses.
+            guesses = processGuess(row: row, col: col, ocean: ocean, guesses: guesses)
+            
+            // Print the updated guesses board, then go to the next move.
+            printBoard(guesses)
+            progress = progress + 1
+        }
     }
+}
+/// Parameter:
+/// - board: The 2D grid to display.
+func printBoard(_ board: [[String]]) {
+    var columnLabels = "  "
+    for i in 1...board.count {
+        columnLabels = columnLabels + "\(i) "
+    }
+    print(columnLabels)
+    
+    for (index, row) in board.enumerated() {
+        var rowString = "\(index + 1) "
+        for cell in row {
+            rowString = rowString + cell + " "
+        }
+        print(rowString)
+    }
+}
+/// Parameters:
+/// - row: The row index for the guess.
+/// - col: The column index for the guess.
+/// - ocean: The hidden ships grid.
+/// - guesses: The player's current guesses grid.
+///
+/// Returns: The updated guesses grid after the guess is applied.
+func processGuess(row: Int, col: Int, ocean: [[String]], guesses: [[String]]) -> [[String]] {
+    // Make sure the row and column exist. If not, exit this function early.
+    guard row >= 1, row <= ocean.count, col >= 1, col <= ocean[0].count else {
+        print("Your guess is invalid. Try again.")
+        return guesses
     }
     
+    // Make sure that the user hasn't already guessed the position.
+    // If not, exit this function early.
+    guard guesses[row - 1][col - 1] != "O" && guesses[row - 1][col - 1] != "X" else {
+        print("You have already guessed that position. Try again.")
+        return guesses
+    }
+    
+    // Make sure that the user hasn't missed the battleship.
+    // If not, return an updated guesses table including the miss.
+    guard ocean[row - 1][col - 1] != "~" else {
+        print("MISS!")
+        var newGuesses = guesses
+        newGuesses[row - 1][col - 1] = "O"
+        return newGuesses
+    }
+    
+    // If the code hasn't returned by now, the player must have hit a ship.
+    print("You've sunk my battleship!")
+    var newGuesses = guesses
+    newGuesses[row - 1][col - 1] = "X"
+    return newGuesses
+}
+/// Parameters:
+/// - ocean: The hidden ships grid.
+/// - guesses: The player's current guesses grid.
+///
+/// Returns: How many ships remain unhit.
+func remainingShips(in ocean: [[String]], guesses: [[String]]) -> Int {
+    var shipsCount = 0
+    for row in 0...ocean.count-1 {
+        for col in 0...row {
+            if ocean[row][col] == "S" {
+                shipsCount = shipsCount + 1
+            }
+        }
+    }
+    
+    var hitCount = 0
+    for row in 0...guesses.count-1 {
+        for col in 0...row {
+            if guesses[row][col] == "X" {
+                hitCount = hitCount + 1
+            }
+        }
+    }
+    
+    return shipsCount - hitCount
+}
+///Creates the board and randomly places ships around it.
+func randomShipping(size: Int, shipCount: Int ) -> [[String]] {
+var randomOcean: [[String]] = Array(repeating: Array(repeating: "~", count: size), count: size)
+for var i in 1...shipCount {
+let row = Int.random (in: 1...size)
+let col = Int.random (in: 1...size)
+if randomOcean[row][col] != "S" {
+    randomOcean[row][col] = "S"
+} else {
+    i = i - 1
+}
 
 }
 
+return randomOcean
+}
 
-    }}
